ba0c9343dd3acc63ba94311250cfe17a4ef62ed2
daleb <dalebarr@gmail.com>
2019-04-04 11:19:18 +1300

    Update activeUsers.R

--- R/activeUsers.R
+++ R/activeUsers.R
@@ -1,9 +1,10 @@
-getActiveUsers <- function() {
-	profile.id <- "79525860" ## Google Analytics View ID (IP Filtered)
-	profileUnfiltered.id <- "118039351" ## (Unfiltered)
-	client.id <- "419497916126-3de9a34jhunanqtlnbcfm09uger88dqb.apps.googleusercontent.com"
-	client.secret <- "L32vmz6aJ_76PfIUgg2RHuSf"
+getActiveUsers <- function(profileId) {
+	#profile.id <- "79525860" ## Google Analytics View ID (IP Filtered)
+	#profileUnfiltered.id <- "118039351" ## (Unfiltered)
+	#client.id <- "419497916126-3de9a34jhunanqtlnbcfm09uger88dqb.apps.googleusercontent.com"
+	#client.secret <- "L32vmz6aJ_76PfIUgg2RHuSf"
 
-	retn_list <- client.id
+	retn_list <- profileId
+
 	retn_list
 }
