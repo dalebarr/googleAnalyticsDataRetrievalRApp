200892fc3c048a52544af73dbe6302bc66dd7611
Dale Barraca <Dale@cff.local>
2019-04-04 09:54:52 +1300

    latest update

--- R/activeUsers.R
+++ R/activeUsers.R
@@ -1,20 +1,9 @@
-#install.packages(c("devtools","RGA","RGoogleAnalytics","RJSONIO"))
-install.packages("tidyverse")
-library(RGA)
-#library(lubridate)
-#library(RGoogleAnalytics)
-install.packages("RGoogleAnalytics")
-library(RGoogleAnalytics)
-library(RJSONIO)
-#library(stats)
 
 getActiveUsers <- function() {
 	profile.id <- "79525860" ## Google Analytics View ID (IP Filtered)
 	profileUnfiltered.id <- "118039351" ## (Unfiltered)
 	client.id <- "419497916126-3de9a34jhunanqtlnbcfm09uger88dqb.apps.googleusercontent.com"
 	client.secret <- "L32vmz6aJ_76PfIUgg2RHuSf"
-
-
 
 	retn_list <- list("profile id" = profile.id,
 	                  "profile id unfiltered" = profileUnfiltered.id,
